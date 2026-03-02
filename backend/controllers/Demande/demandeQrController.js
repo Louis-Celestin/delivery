f06ace03f02fdae160f0c46fffaa8a75a5d46558
devogreen <christian@greenpayci.com>
2026-03-02 09:22:21 +0000

    feat(livraison-QRCODE) : Fonction backend des uploads de QR Codes !

--- backend/controllers/Demande/demandeQrController.js
+++ backend/controllers/Demande/demandeQrController.js
@@ -178,7 +178,7 @@ const faireDemandeQr = async (req, res) => {
             const subject = `NOUVELLE DEMANDE DE QR CODE(S)`;
             let html = `
                 <p>Bonjour,</p>
-                <p>Une nouvelle demande de QR cod(s) a été enregistrée.</p>
+                <p>Une nouvelle demande de QR code(s) a été enregistrée.</p>
                 <ul>
                 <li><strong>Nombre de marchands :</strong> ${quantite_marchand}</li>
                 <li><strong>Nombre de QR Code :</strong> ${quantite_qr}</li>
@@ -272,7 +272,7 @@ const getOneDemandeQr = async (req, res) => {
         const {
             id
         } = req.params
-    
+
         const demande = await prisma.demande_qr.findUnique({
             where: {
                 id: parseInt(id)
@@ -290,10 +290,179 @@ const getOneDemandeQr = async (req, res) => {
         })
         console.log('Succès !')
         res.status(200).json(demande);
-    } catch(error) {
+    } catch (error) {
         console.error("Erreur lors de la récupération de la demande de QR Codes :", error)
         res.status(500).json({ message: "Erreur lors de la récupération de la demande de QR Codes", error });
     }
+
+}
+
+const uploadDemandeQr = async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params
+
+        const {
+            userId,
+            commentaire,
+        } = req.body
+
+        const qrCodes = req.files?.qrCodes || [];
+
+        if(qrCodes.length == 0) {
+            console.error("Erreur : Aucun QR CODE upload")
+            return res.status(400).json({ message: "Aucun QR CODE upload !" });
+        }
+
+        const qrCodes_content = qrCodes.length
+            ? qrCodes.map(file => ({
+                name: file.originalname,
+                path: file.path,
+                type: file.mimetype,
+                size: file.size
+            }))
+            : [];
+
+        const demande = await prisma.demande_qr.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        })
+
+        if (!demande) {
+            console.error("Erreur : demande introuvable")
+            return res.status(404).json({ message: "Demande introuvable !" });
+        }
+
+        const utilisateur = await prisma.users.findUnique({
+            where: {
+                id_user: parseInt(userId)
+            }
+        })
+        if (!utilisateur) {
+            console.error('Utilisteur introuvable !')
+            return res.status(404).json({ message: "Utilisateur introuvable !" });
+        }
+
+        const newGenerationQr = await prisma.$transaction(async (tx) => {
+            const generationForm = await tx.forms.create({
+                data: {
+                    type: 'generation_qr',
+                    created_by: utilisateur.id_user,
+                    created_at: new Date(),
+                    last_modified_at: new Date(),
+                    nom_user: utilisateur.fullname,
+                    commentaire,
+                }
+            });
+            const generation = await tx.generation_qr.create({
+                data: {
+                    form_id: generationForm.id,
+                    quantite_qr: qrCodes.length,
+                    demande_id: demande.id,
+                }
+            })
+            await tx.qr_codes.createMany({
+                data: qrCodes_content.map(file => ({
+                    path: file.path,
+                    filename: file.name,
+                    orignalName: file.name,
+                    mimeType: file.type,
+                    size: file.size,
+                    created_at: new Date(),
+                    uploaded_by: utilisateur.id,
+                    demande_id: demande.id,
+                    generation_id: generation.id,
+                }))
+            })
+
+            await tx.demande_qr.update({
+                where: {
+                    id: demande.id
+                },
+                data: {
+                    statut: 'generee'
+                }
+            })
+
+            return (generationForm, generation)
+        })
+
+        /*****************          GESTION MAIL             *****************/
+
+        const roleImpression = await prisma.user_roles.findMany({
+            where: {
+                role_id: 17,
+            },
+            include: {
+                users: true
+            }
+        })
+        const roleLivraison = await prisma.user_roles.findMany({
+            where: {
+                role_id: 18,
+            },
+            include: {
+                users: true,
+            }
+        })
+        const imprimeurs = roleImpression.map(us => us.users)
+        const livreurs = roleLivraison.map(us => us.users)
+
+        let commentaire_mail = commentaire ? commentaire : '(sans commentaire)';
+
+        const url = GENERAL_URL
+        let link = `${url}/demande-qr-details/${demande.id}`;
+
+        if ((livreurs && livreurs.length > 0) || (imprimeurs && imprimeurs.length > 0)) {
+            const subject = `QR CODE(S) GÉNÉNÉRÉ(S)`;
+            let html = `
+                <p>Bonjour,</p>
+                <p>Les QR Codes de la demande ${demande.id} ont été générés.</p>
+                <ul>
+                <li><strong>Nombre de marchands :</strong> ${quantite_marchand}</li>
+                <li><strong>Nombre de QR Code :</strong> ${quantite_qr}</li>
+                </ul>
+                <br>
+                <p>Commentaire : ${commentaire_mail}<p>
+                <br>
+                <p>Retrouvez la demande à ce lien : 
+                <span>
+                <a href="${link}" target="_blank" style="background-color: #73dced; color: white; padding: 7px 12px; text-decoration: none; border-radius: 5px;">
+                    Cliquez ici !
+                </a>
+                </span>
+                </p>
+                <br><br>
+                <p>Green - Pay vous remercie.</p>
+            `;
+            for (const livreur of livreurs) {
+                await sendMail({
+                    to: livreur.email,
+                    subject,
+                    html,
+                });
+            }
+            for (const imprimeur of imprimeurs) {
+                await sendMail({
+                    to: imprimeur.email,
+                    subject,
+                    html,
+                });
+            }
+        }
+
+        console.log("Génération QR Code éffectué avec succès ! ")
+        res.status(201).json({
+            message: "Génération QR Code éffectué avec succès !",
+            GenerationQr: newGenerationQr,
+        });
+    } catch (error) {
+        console.error("Erreur lors de la génération :", error);
+        res.status(500).json({ message: "Erreur interne", error });
+    }
+
 
 }
 
@@ -302,4 +471,5 @@ module.exports = {
     faireDemandeQr,
     getAllDemandesQr,
     getOneDemandeQr,
+    uploadDemandeQr,
 }
--- backend/routes/demandeQrRoutes.js
+++ backend/routes/demandeQrRoutes.js
@@ -4,12 +4,15 @@ const upload = require("../middlewares/uploads");
 const uploadAll = require("../middlewares/uploadAll")
 
 const {
-    getAllTypePaiement, faireDemandeQr, getAllDemandesQr, getOneDemandeQr,
+    getAllTypePaiement, faireDemandeQr, getAllDemandesQr, getOneDemandeQr, uploadDemandeQr,
 } = require("../controllers/Demande/demandeQrController")
 
 router.get('/getAllTypePaiement', getAllTypePaiement);
 router.post('/faireDemandeQr', faireDemandeQr);
 router.get('/getAllDemandesQr', getAllDemandesQr);
 router.get('/getOneDemandeQr/:id', getOneDemandeQr);
+router.put('/uploadDemandeQr/:id', uploadAll.fields([
+    { name: 'qrCodes'},
+]), uploadDemandeQr)
 
 module.exports = router
